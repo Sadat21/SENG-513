a48a086eea594d24ae46d34e52408a28330b6d40
Sadat Islam <sadat21@hotmail.ca>
2020-02-04 23:51:11 -0700

    Make calculator work with paranthesis

--- Assignment2/Calculator/js/calculator.js
+++ Assignment2/Calculator/js/calculator.js
@@ -1,27 +1,42 @@
-onNormalButtonClick = (character) => {
-    let inputElement = document.getElementById("interim-input");
-    inputElement.value = inputElement.value + character;
-}
+// TODO: Add old value for reuse
+onNormalButtonClick = character => {
+  let inputElement = document.getElementById("interim-input");
+  inputElement.value = inputElement.value + character;
+};
 
 clearInterim = () => {
-    let inputElement = document.getElementById("interim-input");
-    inputElement.value = '';
-}
+  let inputElement = document.getElementById("interim-input");
+  inputElement.value = "";
+};
 
 deleteLast = () => {
-    let inputElement = document.getElementById("interim-input");
-    if (inputElement.value !== '') {
-        inputElement.value = inputElement.value.substring(0, inputElement.value.length - 1);
-    }
-}
+  let inputElement = document.getElementById("interim-input");
+  if (inputElement.value !== "") {
+    inputElement.value = inputElement.value.substring(
+      0,
+      inputElement.value.length - 1
+    );
+  }
+};
 
 equal = () => {
-    let inputElement = document.getElementById("interim-input");
-    let outputElement = document.getElementById("output");
+  let inputElement = document.getElementById("interim-input");
+  let outputElement = document.getElementById("output");
 
-    if (inputElement.value !== '') {
-        const expression = inputElement.value;
-        outputElement.value =  eval(expression);
-        clearInterim();
+  if (inputElement.value !== "") {
+    const expression = inputElement.value;
+    // TODO: Add * between every x(y) and (x)(y)
+    let cleanExpression = expression.replace(/([\d\)])\(/, "$1*(");
+
+    let answer = "";
+    try {
+      // TODO: Scientific value
+      answer = eval(cleanExpression);
+    } catch {
+      answer = "Error";
+    } finally {
+      outputElement.value = expression + "=" + answer;
+      clearInterim();
     }
-}
+  }
+};
